51dd776669b785965667d0ff4c7e5bb5ef455fb2
Tomi <traggio2020@gmail.com>
2025-05-07 22:10:10 -0300

    Sistema Casi fin fin

---
+++ GesSocios/src/main/java/com/unnoba/gessocios/GesSociosApplication.java
@@ -0,0 +1,13 @@
+package com.unnoba.gessocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesSociosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesSociosApplication.class, args);
+    }
+
+}
---
+++ GesSocios/src/test/java/com/unnoba/gessocios/GesSociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unnoba.gessocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesSociosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
